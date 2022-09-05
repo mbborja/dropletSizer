ab8a7101ba8fe1cc619e31981cca005620fa85be
mborj <marcoborja@brandeis.edu>
2022-09-05 00:01:36 -0400

    Added ability to read files

--- LLPSProject.py
+++ LLPSProject.py
@@ -15,8 +15,62 @@ Needs a cleanup
 
 @author: Marco
 """
-import DropletRadiusDetection
+
+#%%
+import matplotlib.pyplot as plt
 import pandas as pd
 import math
-import matplotlib_scalebar.scalebar import ScaleBar
+import DropletRadiusDetection
+from matplotlib_scalebar.scalebar import ScaleBar
+import skimage.filters.thresholding
+from skimage import filters, io, feature, data
+from scipy import ndimage as ndi
+from skimage.morphology import remove_small_objects
+from skimage.filters import try_all_threshold
+from skimage.util import img_as_ubyte
+from skimage.measure import label, regionprops, regionprops_table
+import numpy as np
+import glob
+from skimage import io
+import DropletRadiusDetection
+from DropletRadiusDetection import showFig
 
+path = "C:/Users/mborj/Documents/Rogers Lab/22-08-05 LLPS Data Analysis/Trial 5 0.1mM/"
+conc = "0.1mM"
+files = (glob.glob("C:/Users/mborj/Documents/Rogers Lab/22-08-05 LLPS Data Analysis/Trial 5 0.1mM/*.tif"))
+
+for i in range (25,80):
+    temp = i
+    list = []
+    for filename in files:
+        if str(temp) in filename:
+            list.append(filename)
+    print(list)
+    print(len(list))
+    if len(list) == 2:
+
+        condensates = io.imread(list[0])
+        droplets = io.imread(list[1])
+
+        droplets = droplets[5:1195, 5:1915]
+        condensates = condensates[5:1195, 5:1915]
+
+        # Remove 5 pixels from each image edge
+        droplets = droplets[5:1195, 5:1915]
+        condensates = condensates[5:1195, 5:1915]
+
+        # Apply a Gaussian Filter of Sigma 3 on inner and outer images
+        sig = 3
+        gauss = filters.gaussian(droplets, sigma=sig)
+        condgauss = filters.gaussian(condensates, sigma=(sig - 1))
+
+        # Use a Canny edge detector to detected condensate edges
+        edges = feature.canny(condgauss, sigma=0.5)
+        fill_edges = ndi.binary_fill_holes(edges)
+        fill_edges = remove_small_objects(fill_edges, min_size=200)
+
+        showFig(droplets, title=str(temp) + "C inner")
+
+    # temp = filename[-7:-5]
+    # print(temp)
+    # showFig(io.imread(filename), title = "temp")
